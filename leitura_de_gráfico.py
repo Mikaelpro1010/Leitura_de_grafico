6d8dd7347baa132a5f49b99ef716b6c8025f258e
Mikael Aguiar <93383052+Mikaelpro1010@users.noreply.github.com>
2022-04-27 16:59:09 -0300

    Add files via upload

---
+++ leitura_de_gráfico.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+df = pd.read_excel('C:\\Users\\Prefeitura\\Desktop\\MIkael\\Leitura_de_gráficos\\Resultado_0 -Indicadores - 0072 FORTALECIMENTO E INOVAÇÃO DA GESTÃO EM SAÚDE ok- Resultado.xlsx')
+
+df.head()
